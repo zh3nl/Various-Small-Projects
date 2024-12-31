50b83c664465ca9e003edc49ed736f89982f3e2d
Zhen Liu <127721103+zh3nl@users.noreply.github.com>
2024-12-30 18:02:51 -0800

    Add files via upload

---
+++ React Native/components/screens/City.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import {
+  SafeAreaView,
+  Text,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
+import IconText from "../iconText";
+import moment from "moment";
+
+const City = ({ weatherData }) => {
+  const {
+    container,
+    cityName,
+    cityText,
+    countryName,
+    populationWrapper,
+    populationText,
+    riseSetWrapper,
+    riseSetText,
+    rowLayout,
+    imageLayout,
+  } = styles;
+  const {name, country, population, sunrise, sunset} = weatherData; 
+
+  return (
+    <SafeAreaView style={container}>
+      <ImageBackground
+        source={require("../../assets/images/city-background.jpg")}
+        style={imageLayout}
+      >
+        <Text style={[cityName, cityText]}>{name}</Text>
+        <Text style={[countryName, cityText]}>{country}</Text>
+        <View style={[rowLayout, populationWrapper]}>
+          <IconText
+            iconName={"user"}
+            iconColor={"red"}
+            bodyText={`Population: ${population}`}
+            bodyTextStyles={populationText}
+          />
+        </View>
+        <View style={[rowLayout, riseSetWrapper]}>
+          <IconText
+            iconName={"sunrise"}
+            iconColor={"white"}
+            bodyText={moment(sunrise).format("h:mm:ss a")}
+            bodyTextStyles={riseSetText}
+          />
+          <IconText
+            iconName={"sunset"}
+            iconColor={"white"}
+            bodyText={moment(sunset).format("h:mm:ss a")}
+            bodyTextStyles={riseSetText}
+          />
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  imageLayout: {
+    flex: 1,
+  },
+  cityName: {
+    fontSize: 40,
+  },
+  countryName: {
+    fontSize: 30,
+  },
+  cityText: {
+    justifyContent: "center",
+    alignSelf: "center",
+    color: "white",
+    fontWeight: "bold",
+  },
+  populationWrapper: {
+    justifyContent: "center",
+    marginTop: 30,
+  },
+  populationText: {
+    fontSize: 20,
+    marginLeft: 7.5,
+    color: "red",
+  },
+  riseSetWrapper: {
+    justifyContent: "space-around",
+    marginTop: 30,
+  },
+  riseSetText: {
+    fontSize: 15,
+    color: "white",
+  },
+  rowLayout: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
+
+export default City;
