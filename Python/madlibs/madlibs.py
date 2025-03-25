666202a090422be785632336c9a4b2cbb16b2160
zh3nl <zhenliu200677@gmail.com>
2025-03-24 23:28:28 -0700

    quick warmup

---
+++ Python/madlibs/madlibs.py
@@ -0,0 +1,18 @@
+# String Concatenation
+# Suppose we want to create a string that says "subscribe to __"
+
+# youtuber = "Zhen"; 
+
+# Some ways to do this
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
