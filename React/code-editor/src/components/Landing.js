65c416a79d210ac11fd4cb934aea03607914e09b
zh3nl <zhenliu200677@gmail.com>
2025-03-24 23:13:13 -0700

    finished final features

--- React/code-editor/src/components/Landing.js
+++ React/code-editor/src/components/Landing.js
@@ -3,8 +3,10 @@ import CodeEditorWindow from "./CodeEditorWindow";
 import axios from "axios";
 import { classnames } from "../utils/general";
 import { languageOptions } from "../constants/languageOptions";
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import { defineTheme } from "../lib/defineTheme";
 import useKeyPress from "../hooks/useKeyPress";
 import Footer from "./Footer";
@@ -14,7 +16,8 @@ import OutputDetails from "./OutputDetails";
 import ThemeDropdown from "./ThemeDropdown";
 import LanguagesDropdown from "./LanguagesDropdown";
 
-const javascriptDefault = `// some comment`;
+const javascriptDefault = `// Some comments here
+`;
 
 const Landing = () => {
   const [code, setCode] = useState(javascriptDefault);
@@ -28,16 +31,17 @@ const Landing = () => {
   const ctrlPress = useKeyPress("Control");
 
   const onSelectChange = (sl) => {
-    console.log("selected option...", sl);
+    console.log("selected Option...", sl);
     setLanguage(sl);
   };
 
   useEffect(() => {
     if (enterPress && ctrlPress) {
+      console.log("enterPress", enterPress);
+      console.log("ctrlPress", ctrlPress);
       handleCompile();
     }
   }, [ctrlPress, enterPress]);
-
   const onChange = (action, data) => {
     switch (action) {
       case "code": {
@@ -45,60 +49,105 @@ const Landing = () => {
         break;
       }
       default: {
-        console.warn("case not handled", action, data);
+        console.warn("case not handled!", action, data);
       }
     }
   };
-
   const handleCompile = () => {
-    setProcessing(true); 
+    setProcessing(true);
     const formData = {
       language_id: language.id,
-      
       // encode source code in base64
       source_code: btoa(code),
-      stdin: btoa(customInput)
+      stdin: btoa(customInput),
     };
-
     const options = {
       method: "POST",
       url: process.env.REACT_APP_RAPID_API_URL,
-      params: { base64_encoded: "true", fields: ""},
-      headers : {
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
         "content-type": "application/json",
         "Content-Type": "application/json",
         "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-        "X-RapidAPI_Key": process.env.REACT_APP_RAPID_API_KEY,
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
       },
       data: formData,
     };
 
-    axios.request(options).then(function (response) {
-      console.log("res.data", response.data);
-      const token = response.data.token;
-      checkStatus(token); 
-    }).catch((err) => {
-      let error = err.response ? err.response.data : err;
-      setProcessing(false); 
-      console.log(error); 
-    });
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log("res.data", response.data);
+        const token = response.data.token;
+        checkStatus(token);
+      })
+      .catch((err) => {
+        let error = err.response ? err.response.data : err;
+        // get error status
+        let status = err.response.status;
+        console.log("status", status);
+        if (status === 429) {
+          console.log("too many requests", status);
+
+          showErrorToast(
+            `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
+            10000
+          );
+        }
+        setProcessing(false);
+        console.log("catch block...", error);
+      });
   };
 
   const checkStatus = async (token) => {
     const options = {
-      
+      method: "GET",
+      url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
+      params: { base64_encoded: "true", fields: "*" },
+      headers: {
+        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+      },
+    };
+    try {
+      let response = await axios.request(options);
+      let statusId = response.data.status?.id;
+
+      // Processed - we have a result
+      if (statusId === 1 || statusId === 2) {
+        // still processing
+        setTimeout(() => {
+          checkStatus(token);
+        }, 2000);
+        return;
+      } else {
+        setProcessing(false);
+        setOutputDetails(response.data);
+        showSuccessToast(`Compiled Successfully!`);
+        console.log("response.data", response.data);
+        return;
+      }
+    } catch (err) {
+      console.log("err", err);
+      setProcessing(false);
+      showErrorToast();
     }
-    return;
   };
 
-  const handleThemeChange = (th) => {
-    return;
-  };
+  function handleThemeChange(th) {
+    const theme = th;
+    console.log("theme...", theme);
 
+    if (["light", "vs-dark"].includes(theme.value)) {
+      setTheme(theme);
+    } else {
+      defineTheme(theme.value).then((_) => setTheme(theme));
+    }
+  }
   useEffect(() => {
-    defineTheme("oceanic-next").then((_) => {
-      setTheme({ value: "oceanic-next", label: "Oceanic Next" });
-    });
+    defineTheme("oceanic-next").then((_) =>
+      setTheme({ value: "oceanic-next", label: "Oceanic Next" })
+    );
   }, []);
 
   const showSuccessToast = (msg) => {
@@ -112,16 +161,15 @@ const Landing = () => {
       progress: undefined,
     });
   };
-
-  const showErrorToast = (msg) => {
-    toast.error(msg || `Something went wrong. Please try again later.`, {
+  const showErrorToast = (msg, timer) => {
+    toast.error(msg || `Something went wrong! Please try again.`, {
       position: "top-right",
-      autoClose: 1000,
+      autoClose: timer ? timer : 1000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
-      progess: undefined,
+      progress: undefined,
     });
   };
 
@@ -156,6 +204,7 @@ const Landing = () => {
             theme={theme.value}
           />
         </div>
+
         <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
           <OutputWindow outputDetails={outputDetails} />
           <div className="flex flex-col items-end">
@@ -181,5 +230,4 @@ const Landing = () => {
     </>
   );
 };
-
 export default Landing;
